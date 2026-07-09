3d51c4bf0650c229164a0fd18ce59764ad716c36
AntikSurfWebSite <pnaredobernardos@gmail.com>
2026-07-09 22:55:18 +0800

    Quito tarjetas, anado mensaje de intro tras la portada y seccion upcoming surftrips; pendiente anadir info de lessons y rentals debajo

--- script.js
+++ script.js
@@ -3,31 +3,14 @@ let menuBoton = document.getElementById("menuBoton");
 let menuCerrar = document.getElementById("menuCerrar");
 let menuDesplegable = document.getElementById("menuDesplegable");
 let menuLinks = document.querySelectorAll("#menuDesplegable a");
-let hero = document.getElementById("hero");
 let surfLessonsTrack = document.getElementById("surfLessonsTrack");
 let surfLessonsPrev = document.getElementById("surfLessonsPrev");
 let surfLessonsNext = document.getElementById("surfLessonsNext");
-
-const imagenesHero = [
-    'images/fondo.webp',
-    'images/surflessons.jpg',
-    'images/surfrentals.jpg',
-    'images/surftrips.jpg'
-];
-
-let indiceImagen = 0;
 
 function cerrarMenu() {
     if (!menuDesplegable) return;
     menuDesplegable.classList.remove("abierto");
     document.body.style.overflow = "auto";
-}
-
-function cambiarImagenHero() {
-    if (!hero) return;
-
-    indiceImagen = (indiceImagen + 1) % imagenesHero.length;
-    hero.style.backgroundImage = `url('${imagenesHero[indiceImagen]}')`;
 }
 
 if (menuBoton && menuDesplegable) 
@@ -51,8 +34,6 @@ if (menuLinks.length > 0) {
         });
     });
 }
-
-setInterval(cambiarImagenHero, 6000);
 
 if (surfLessonsTrack && surfLessonsPrev && surfLessonsNext) {
     const slideCount = 3;
